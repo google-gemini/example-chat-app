0231060fd601bf154348101c1f15b1c6d8b33331
Prianka Liz Kariat <prianka.kariat@codeandtheory.com>
2024-10-04 18:24:59 +0530

    Updated documentation of json.go

--- server-go/json.go
+++ server-go/json.go
@@ -1,3 +1,20 @@
+/**
+ * @license
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package main
 
 import (
@@ -7,6 +24,9 @@ import (
 	"net/http"
 )
 
+// parseRequestJSON populates the target with the fields of the JSON-encoded value in the request
+// body. It expects the request to have the Content-Type header set to JSON and a body with a
+// JSON-encoded value complying with the underlying type of target.
 func parseRequestJSON(r *http.Request, target any) error {
 	contentType := r.Header.Get("Content-Type")
 	mediaType, _, err := mime.ParseMediaType(contentType)
@@ -23,6 +43,7 @@ func parseRequestJSON(r *http.Request, target any) error {
 	return dec.Decode(target)
 }
 
+// renderResponseJSON encodes res as JSON and writes it to w.
 func renderResponseJSON(w http.ResponseWriter, res any) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(res)
